a311d5215eb807a3543b5f59072c25e9512425a5
Sylvain Breton <sbreton@devolutions.net>
2022-05-24 10:24:13 -0400

    mock updated

---
+++ src/app/mock-badasses.ts
@@ -0,0 +1,7 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 4, isBold: true, name: 'FunkyChicken' },
+    { id: 5, isBold: false, name: 'Anonymous Boh' },
+    { id: 6, isBold: true, name: 'MentalDentist' },
+];
