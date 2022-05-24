de5032b47dd607d0963bf308d9470b0369f85421
Sylvain Breton <sylvain.breton@gmail.com>
2022-05-24 11:27:42 -0400

    number ajusted in mocks

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -4,8 +4,8 @@ export const HEROES: Hero[] = [
     { id: 1, isBold: false, name: 'Marius' },
     { id: 2, isBold: true, name: 'Dr. Nice' },
     { id: 3, isBold: true, name: 'Bombasto' },
-    { id: 7, isBold: false, name: 'Dynama' },
-    { id: 8, isBold: false, name: 'Dr. IQ' },
-    { id: 9, isBold: false, name: 'Dr. MattSenior' },
-    { id: 0, isBold: false, name: 'Tornado' }
+    { id: 4, isBold: false, name: 'Dynama' },
+    { id: 5, isBold: false, name: 'Dr. IQ' },
+    { id: 6, isBold: false, name: 'Dr. MattSenior' },
+    { id: 7, isBold: false, name: 'Tornado' }
 ];
